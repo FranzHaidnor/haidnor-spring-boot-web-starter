9dcaf63e1470c0f7b62a1f451251ff832a7a50cc
Franz Haidnor <haidnor@outlook.com>
2023-12-09 10:42:02 +0800

    update

--- src/main/java/haidnor/web/http/Result.java
+++ src/main/java/haidnor/web/http/Result.java
@@ -2,38 +2,40 @@ package haidnor.web.http;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.util.Objects;
+
 public class Result<T> {
 
     /**
      * 成功状态码
      */
-    public static final Integer SUCCEED_CODE = 0;
+    public static Integer SUCCEED_CODE = 0;
 
     /**
      * 失败状态码
      */
-    public static final Integer FAIL_CODE = 1;
+    public static Integer FAIL_CODE = 1;
 
     /**
      * 响应码. 0 成功, 1 失败
      */
-    private final Integer code;
+    private Integer code;
 
     /**
      * 异常堆栈信息
      */
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private final String stack;
+    private String stack;
 
     /**
      * 接口信息
      */
-    private final String message;
+    private String message;
 
     /**
      * 响应数据
      */
-    private final T data;
+    private T data;
 
 
     private Result(Integer code, String message, T data, String stack) {
@@ -63,19 +65,66 @@ public class Result<T> {
         return new Result<>(FAIL_CODE, message, null, stack);
     }
 
+    public static Integer getSucceedCode() {
+        return SUCCEED_CODE;
+    }
+
+    public static void setSucceedCode(Integer succeedCode) {
+        SUCCEED_CODE = succeedCode;
+    }
+
+    public static Integer getFailCode() {
+        return FAIL_CODE;
+    }
+
+    public static void setFailCode(Integer failCode) {
+        FAIL_CODE = failCode;
+    }
+
     public Integer getCode() {
         return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
     }
 
     public String getStack() {
         return stack;
     }
 
+    public void setStack(String stack) {
+        this.stack = stack;
+    }
+
     public String getMessage() {
         return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 
     public T getData() {
         return data;
     }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public boolean isSucceed() {
+        return Objects.equals(this.code, SUCCEED_CODE);
+    }
+
+    /**
+     * 检查响应结果, 如果失败了, 则抛出异常
+     */
+    public T get() {
+        if (Objects.equals(this.code, FAIL_CODE)) {
+            throw new RuntimeException(message);
+        }
+        return data;
+    }
+
 }
